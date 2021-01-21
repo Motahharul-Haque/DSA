a331f557d21993bdc868b1f6f31aaf0a3087f970
MD MOTAHHARUL HAQUE <mdmotahharul.haque@gmail.com>
2021-01-22 00:49:52 +0530

    Collection

---
+++ Algorithms/src/main/java/Sorting/Collection/SortMethod/Collectionsorting.java
@@ -0,0 +1,26 @@
+package Sorting.Collection.SortMethod;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Collectionsorting
+{
+    public static void main(String[] args)
+    {
+
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(5);
+        list.add(20);
+        
+
+        Collections.sort(list);
+
+        System.out.println(list);
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        System.out.println(list);
+    }
+}
