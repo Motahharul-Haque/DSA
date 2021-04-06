28ebd59f2bee2874094c9034b1f20e59665aeb22
MD MOTAHHARUL HAQUE <mdmotahharul.haque@gmail.com>
2021-04-07 01:18:22 +0530

    recursive reverse LL by method-2

---
+++ Data_Structures/src/main/java/Problems/Recursive_Rev_LL_Methosd2.java
@@ -0,0 +1,31 @@
+package Problems;
+
+public class Recursive_Rev_LL_Methosd2 {
+
+    public static void main(String[] args) {
+        Node head=new Node(10);
+        head.next=new Node(20);
+        head.next.next=new Node(30);
+        printlist(head);
+        head=recRevL(head,null);
+        printlist(head);
+    }
+
+    private static Node recRevL(Node curr, Node prev ) {
+        if (curr == null) {
+            return prev;
+        }
+        Node next = curr.next;
+        curr.next = prev;
+        return recRevL(next, curr);
+    }
+
+
+    public static void printlist(Node head){
+        Node curr=head;
+        while(curr!=null){
+            System.out.print(curr.data+" ");
+            curr=curr.next;
+        }System.out.println();
+    }
+}
