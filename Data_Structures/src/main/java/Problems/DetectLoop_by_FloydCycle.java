8fb1b38abc6997a2ecc05598c08c5da54bb6ca0b
MD MOTAHHARUL HAQUE <mdmotahharul.haque@gmail.com>
2021-05-18 00:55:03 +0530

    detect loop by floyd cycle algorithm

---
+++ Data_Structures/src/main/java/Problems/DetectLoop_by_FloydCycle.java
@@ -0,0 +1,40 @@
+package Problems;
+
+import java.util.HashSet;
+
+public class DetectLoop_by_FloydCycle {
+    public static void main(String[] args) {
+        Node head=new Node(15);
+        head.next=new Node(10);
+        head.next.next=new Node(12);
+        head.next.next.next=new Node(20);
+        head.next.next.next.next=head.next;
+        if (isLoop(head))
+            System.out.print("Loop found");
+        else
+            System.out.print("No Loop");
+    }
+
+    private static boolean isLoop(Node head)
+    {
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static void printlist(Node head){
+        Node curr=head;
+        while(curr!=null){
+            System.out.print(curr.data+" ");
+            curr=curr.next;
+        }System.out.println();
+    }
+}
