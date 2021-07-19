6a32a171b6dba0a3c8894b7e2bd91d3ce5a7654d
Motahharul-Haque <mdmotahharul.haque@gmail.com>
2021-07-20 01:36:22 +0530

    implementation of k stack in an array

---
+++ Data_Structures/src/main/java/Stack/KStacks.java
@@ -0,0 +1,84 @@
+package Stack;
+
+public class KStacks {
+    int[] arr;
+    int[] top;
+    int[] next;
+    int cap, k;
+    int freeTop;
+
+    KStacks(int k1, int n)
+    {
+        k = k1;
+        cap = n;
+        arr = new int[cap];
+        top = new int[k];
+        next = new int[cap];
+
+        for (int i = 0; i < k; i++) {
+            top[i] = -1;
+        }
+        freeTop = 0;
+        for (int i = 0; i < cap - 1; i++) {
+            next[i] = i + 1;
+        }
+        next[cap - 1] = -1;
+    }
+
+    boolean isFull(){
+        return (freeTop == -1);
+    }
+
+    boolean isEmpty(int sn)
+    {
+        return (top[sn] == -1);
+    }
+
+    void push(int x, int sn)
+    {
+        if (isFull()) {
+            System.out.println("\nStack Overflow\n");
+        }
+
+        int i = freeTop;
+        freeTop = next[i];
+        next[i] = top[sn];
+        top[sn] = i;
+        arr[i] = x;
+    }
+
+    int pop(int sn)
+    {
+        if (isEmpty(sn)) {
+            System.out.println("\nStack Underflow\n");
+            return Integer.MAX_VALUE;
+        }
+
+        int i= top[sn];
+        top[sn] = next[i];
+        freeTop = i;
+        return arr[i];
+    }
+}
+
+class TestKStack
+{
+    public static void main(String[] args) {
+        int k = 3, n = 10;
+        KStacks ks = new KStacks(k, n);
+
+        ks.push(15, 2);
+        ks.push(45, 2);
+
+        ks.push(17, 1);
+        ks.push(49, 1);
+        ks.push(39, 1);
+        ks.push(11, 0);
+        ks.push(9, 0);
+        ks.push(7, 0);
+
+        System.out.println("Popped element form stack 2 is : "+ ks.pop(2));
+        System.out.println("Popped element form stack 1 is : "+ ks.pop(1));
+        System.out.println("Popped element form stack 0 is : "+ ks.pop(0));
+    }
+}
