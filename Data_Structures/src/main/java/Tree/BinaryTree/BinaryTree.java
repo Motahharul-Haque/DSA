f55ae18fe1b31f4df2a77e7155c6e3c4bab1be74
Motahharul-Haque <mdmotahharul.haque@gmail.com>
2021-09-10 01:09:34 +0530

    Binary Tree Traversal

---
+++ Data_Structures/src/main/java/Tree/BinaryTree/BinaryTree.java
@@ -0,0 +1,44 @@
+package Tree.BinaryTree;
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.right.right = new Node(50);
+
+        //In-Order Traversal of a binary tree
+        inorder(root);
+        //Post-Order Traversal of a binary tree
+        //postorder(root);
+        //Pre-Order Traversal of a binary tree
+        //preorder(root);
+
+    }
+
+    private static void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.key + " ");
+            inorder(root.right);
+        }
+    }
+
+    /*private static void postorder(Node root) {
+        if (root != null) {
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.key + " ");
+        }
+    }*/
+
+    /*private static void preorder(Node root) {
+        if (root != null) {
+            System.out.print(root.key + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }*/
+
+}
