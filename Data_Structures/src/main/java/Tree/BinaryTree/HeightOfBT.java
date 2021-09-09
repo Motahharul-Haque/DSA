c2fbfc54695dba1c54e041b7a45a0b6c9424e762
Motahharul-Haque <mdmotahharul.haque@gmail.com>
2021-09-10 01:48:24 +0530

    Binary Tree based problem

---
+++ Data_Structures/src/main/java/Tree/BinaryTree/HeightOfBT.java
@@ -0,0 +1,23 @@
+package Tree.BinaryTree;
+
+public class HeightOfBT {
+
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.right.left = new Node(40);
+        root.right.right = new Node(50);
+
+        System.out.println(height(root));
+    }
+
+    private static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        else {
+            return (1 + Math.max(height(root.left), height(root.right)));
+        }
+    }
+}
