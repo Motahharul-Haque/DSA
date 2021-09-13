e70fec5e7af398beffd4355d47552aa1e36fa412
Motahharul-Haque <mdmotahharul.haque@gmail.com>
2021-09-14 01:54:19 +0530

    Size of a binary tree

---
+++ Data_Structures/src/main/java/Tree/BinaryTree/SizeOfBT.java
@@ -0,0 +1,24 @@
+package Tree.BinaryTree;
+
+public class SizeOfBT {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+        root.right.right = new Node(60);
+
+        System.out.println("Size of the Binary Tree "+ getSize(root));
+
+    }
+
+    private static int getSize(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        else {
+            return 1 + getSize(root.left)+getSize(root.right);
+        }
+    }
+}
