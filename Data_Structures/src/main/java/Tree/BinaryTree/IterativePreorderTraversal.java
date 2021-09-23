52f6bb927234c9a5bedba4446ad6546fb7df2048
Motahharul-Haque <mdmotahharul.haque@gmail.com>
2021-09-24 03:21:57 +0530

    Iterative preorder traversal of binary tree

--- Data_Structures/src/main/java/Tree/BinaryTree/IterativePreorderTraversal.java
+++ Data_Structures/src/main/java/Tree/BinaryTree/IterativePreorderTraversal.java
@@ -1,16 +1,7 @@
 package Tree.BinaryTree;
 
 import java.util.Stack;
-tree.root = new Node(10);
-        tree.root.left = new Node(20);
-        tree.root.right = new Node(30);
-        tree.root.left.left = new Node(40);
-        tree.root.left.left.left = new Node(70);
-        tree.root.left.right = new Node(50);
-        tree.root.right.left = new Node(60);
-        tree.root.left.left.right = new Node(80);
 
-        tree.preorderIterative();
 public class IterativePreorderTraversal {
     public static void main(String[] args) {
         Node root = new Node(10);
